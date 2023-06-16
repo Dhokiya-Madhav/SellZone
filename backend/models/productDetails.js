beb00593da2cd8675bea44f34d38f506907662de
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-06-16 19:39:57 +0530

    Schema changes

--- backend/models/productDetails.js
+++ backend/models/productDetails.js
@@ -7,30 +7,12 @@ var productSchema = new mongoose.Schema({
     product_price: Number,
     state:String,
     city:String,
-    img1:
-    {
-        data: Buffer,
-        contentType: String
-    },
-    img2:
-    {
-        data: Buffer,
-        contentType: String
-    },
-    img3:
-    {
-        data: Buffer,
-        contentType: String
-    },
-    img4:
-    {
-        data: Buffer,
-        contentType: String
-    }
+    img: String
 },
 {
     collection: "product-details",
 }
 );
 
-module.exports = mongoose.model('ProductInfo', productSchema);
+const product = mongoose.model('ProductInfo', productSchema);
+module.exports={product}
