1b508e0560bc6b63947f02c05a49e5737ace4c0e
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-06-17 15:33:26 +0530

    Product UI updated

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -12,25 +12,26 @@ export default function Home() {
     }, []);
 
     const p_data = productData.map(
-        (element)=>{
-            return(
+        (element) => {
+            return (
                 <div className="col">
-                    <div className="card h-100">
-                        <img src={element.img}  className="card-img-top img-fluid" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">{element.product_title}</h5>
-                                <p className="card-text">#{element.product_type}</p>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted"><Link to="/productDetails" state={{id:element._id}}><button className="btn btn-outline-danger">More details</button></Link></small>
-                            </div>
+                    <div className="card p-3 border-1 border-danger">
+                        <img src={element.img} style={{height:250,width:280}} className="card-img-top text-center img-fluid" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{element.product_title}</h5>
+                            <p className="card-text">#{element.product_type}</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted"><Link to="/productDetails" state={{ id: element._id }}><button className="btn btn-outline-danger">More details</button></Link></small>
+                        </div>
                     </div>
                 </div>
             )
         })
     return (
         <>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
+            <br></br>
+            <div className="row row-cols-1 row-cols-md-4 g-4">
                 {p_data}
             </div>
         </>
