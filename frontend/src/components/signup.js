57ab8842283ad686d218ccf4d6797fc54337f2c3
Dhokiya-Madhav <madhavdhokiya@gmail.com>
2023-09-14 21:18:53 +0530

    Validation added for signup component

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -2,28 +2,78 @@ import React from "react";
 import { useState } from "react";
 export default function SignUp() {
     const [usrName, setUserName] = useState("");
+    const [usrNameErrorMsg, setUserNameErrorMsg] = useState("");
     const [u_email, setEmail] = useState("");
+    const [emailErrorMsg, setEmailErrorMsg] = useState("");
     const [psw, setPsw] = useState("");
+    const [passwordErrorMsg, setPasswordErrorMsg] = useState("");
     const [cpsw, csetPsw] = useState("");
+    const [cnfPasswordErrorMsg, setCnfPasswordErrorMsg] = useState("");
     const [phone, setPhone] = useState("");
-    const validate = (e) => {
-        e.preventDefault();
-        var check = 0;
-        if (psw.length <= 5) {
-            alert("Length of password should be greater than 6");
-            check = 1;
-        }
-        if (psw != cpsw) {
-            alert("Password not matched");
-            check = 1;
-        }
-        if (phone.length != 10) {
-            alert("Enter valid phone number");
-            check = 1;
-        }
+    const [phoneNumberErrorMsg, setPhoneNumberErrorMsg] = useState("");
 
-        if (check == 0) 
-        {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
+
+    const userNameValidate = (e) => {
+        const userName = e.target.value;
+        setUserName(userName);
+        if (userName.length < 4) {
+            setUserNameErrorMsg("Username should be greater than 4 character")
+        }
+        else {
+            setUserNameErrorMsg("");
+        }
+    }
+
+    const emailValidate = (e) => {
+        const emailId = e.target.value;
+        setEmail(emailId);
+        if (!emailRegex.test(emailId)) {
+            setEmailErrorMsg("Invalid email address")
+        }
+        else {
+            setEmailErrorMsg("");
+        }
+    }
+
+    const passwordValidate = (e) => {
+        const userPassword = e.target.value;
+        setPsw(userPassword)
+        if (!passwordRegex.test(userPassword)) {
+            setPasswordErrorMsg(
+                "Invalid password. It should be at least 6 characters long and include alphabets, numbers, and special characters."
+            );
+        }
+        else {
+            setPasswordErrorMsg("");
+        }
+    }
+
+    const phoneNumberValidate = (e) => {
+        const userPhoneNumber = e.target.value;
+        setPhone(userPhoneNumber);
+        if (userPhoneNumber.length != 10) {
+            setPhoneNumberErrorMsg("Invalid phone number")
+        }
+        else {
+            setPhoneNumberErrorMsg("");
+        }
+    }
+
+    const passwordMatching = (e) => {
+        const cnfPassword = e.target.value;
+        csetPsw(cnfPassword);
+        if (psw != cnfPassword) {
+            setCnfPasswordErrorMsg("Password does not match")
+        } else {
+            setCnfPasswordErrorMsg("");
+        }
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        if ((usrNameErrorMsg == "" && emailErrorMsg == "" && passwordErrorMsg == "" && cnfPasswordErrorMsg == "" && phoneNumberErrorMsg == "") && (usrName != "" && u_email != "" && psw != "" && cpsw != "" && phone != "")) {
             fetch("http://localhost:5000/registerUser", {
                 method: "POST",
                 crossDomain: true,
@@ -44,8 +94,8 @@ export default function SignUp() {
                     console.log(data);
                     if (data.status === "ok") {
                         alert("Sign Up Successful");
-                        window.location="http://localhost:3000/login";
-                    } else if(data.error === "User Exists"){
+                        window.location = "http://localhost:3000/login";
+                    } else if (data.error === "User Exists") {
                         alert("User already exists");
                     }
                 });
@@ -60,35 +110,49 @@ export default function SignUp() {
                             <div className="col-12 col-md-9 col-lg-7 col-xl-6">
                                 <div className="card">
                                     <div className="card-body p-5">
-                                        <form onSubmit={validate}>
+                                        <form>
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form3Example1cg">Username</label>
-                                                <input type="text" id="form3Example1cg" onChange={(e) => setUserName(e.target.value)} className="form-control" required />
+                                                <input type="text" id="form3Example1cg" onChange={userNameValidate} className="form-control" required />
+                                                {usrNameErrorMsg && (
+                                                    <div className="text-danger">{usrNameErrorMsg}</div>
+                                                )}
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form3Example3cg">Email Id</label>
-                                                <input type="email" id="form3Example3cg" onChange={(e) => setEmail(e.target.value)} className="form-control" required />
+                                                <input type="email" id="form3Example3cg" onChange={emailValidate} className="form-control" required />
+                                                {emailErrorMsg && (
+                                                    <div className="text-danger">{emailErrorMsg}</div>
+                                                )}
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form3Example4cg">Password</label>
-                                                <input type="password" id="form3Example4cg" onChange={(e) => setPsw(e.target.value)} className="form-control" required />
-
+                                                <input type="password" id="form3Example4cg" onChange={passwordValidate} className="form-control" required />
+                                                {passwordErrorMsg && (
+                                                    <div className="text-danger">{passwordErrorMsg}</div>
+                                                )}
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form3Example4cdg">Confirm password</label>
-                                                <input type="password" id="form3Example4cdg" onChange={(e) => csetPsw(e.target.value)} className="form-control" required />
+                                                <input type="password" id="form3Example4cdg" onChange={passwordMatching} className="form-control" required />
+                                                {cnfPasswordErrorMsg && (
+                                                    <div className="text-danger">{cnfPasswordErrorMsg}</div>
+                                                )}
                                             </div>
 
                                             <div className="form-outline mb-4">
                                                 <label className="form-label" for="form3Example3cg">Phone No</label>
-                                                <input type="number" id="form3Example3cg" className="form-control" onChange={(e) => setPhone(e.target.value)} required />
+                                                <input type="number" id="form3Example3cg" className="form-control" onChange={phoneNumberValidate} required />
+                                                {phoneNumberErrorMsg && (
+                                                    <div className="text-danger">{phoneNumberErrorMsg}</div>
+                                                )}
                                             </div>
 
                                             <div className="d-flex justify-content-center">
-                                                <button type="Submit"
+                                                <button type="Submit" onClick={handleSignup}
                                                     className="btn btn-danger btn-block text-white">Register</button>
                                             </div>
                                         </form>
